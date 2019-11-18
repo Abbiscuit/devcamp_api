71284b0bcf25af32269f4f43217fa268042e0010
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-11-19 07:48:44 +0900

    Add basic routes of devcamp

--- server.js
+++ server.js
@@ -6,6 +6,52 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+// Routes
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: {
+      msg: "Show All Bootcamps"
+    }
+  });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: {
+      msg: `Show bootcamp #${req.params.id}`
+    }
+  });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: {
+      msg: "Create All Bootcamps"
+    }
+  });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: {
+      msg: `Update bootcamp #${req.params.id}`
+    }
+  });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: {
+      msg: `Delete bootcamp #${req.params.id}`
+    }
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
